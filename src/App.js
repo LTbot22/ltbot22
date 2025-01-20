f49fc952f47640ce6fb2f25544bc21c6daa5706c
LTbot22 <sunsanxiao9@gmail.com>
2025-01-21 05:07:16 +0800

    更新说明

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ChatInterface from './components/ChatInterface';
 import './App.css';
+import techBg from './assets/images/tech-bg.jpg';
 
 function App() {
   return (
-    <div className="app">
+    <div className="app" style={{ backgroundImage: `url(${techBg})` }}>
       <header className="app-header">
         <h1>AI聊天BOT（测试版）</h1>
       </header>
